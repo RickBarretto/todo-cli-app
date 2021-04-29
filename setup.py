280aeccdbff8479c3449c83068e474437e9554ba
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2021-04-29 19:37:39 -0300

    minor improvements

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ import setuptools
 setuptools.setup(
     name='todo',
     version='0.0.1',
-    scripts=['./scripts/myscript'],
     author='Me',
     description='This runs my script which is great.',
-    packages=['lib.myscript']
+    packages=['todo_cli_app'],
     install_requires=[
         'setuptools'
     ],
